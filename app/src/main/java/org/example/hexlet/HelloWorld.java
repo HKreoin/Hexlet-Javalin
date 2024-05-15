efd424b5d274590daf1a330073046f0451286b87
HKreoin <steamuser85@yandex.ru>
2024-05-15 22:44:56 +0300

    add path parameters

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,6 +10,9 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/hello", ctx -> ctx.result("Hello " + ctx.queryParamAsClass("name", String.class).getOrDefault("World") + "!"));
+        app.get("users/{id}/post/{postId}", ctx -> {
+            ctx.result("id: " + ctx.pathParam("id") + " postId: " + ctx.pathParam("postId"));
+        });
         app.start(7070); // Стартуем веб-сервер
     }
 }
