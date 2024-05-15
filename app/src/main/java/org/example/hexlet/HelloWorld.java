642de84920f91fa8e2e92829b12c3107570111c0
HKreoin <steamuser85@yandex.ru>
2024-05-15 21:39:22 +0300

    modify helloworld app, add make start

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
         });
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> ctx.result("Hello " + ctx.queryParamAsClass("name", String.class).getOrDefault("World") + "!"));
         app.start(7070); // Стартуем веб-сервер
     }
 }
